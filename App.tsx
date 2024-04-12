6c0bfe36ffbc98487d2a767c4912787414663cc1
Konradin Schrader <k.schrader@hm.edu>
2024-04-12 12:10:48 +0200

    navbar und env ergänzt

--- App.tsx
+++ App.tsx
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import  * as React from 'react';
+import Home from './screens/Home';
+import NewPlant from './screens/NewPlant';
+import MyPlants from './screens/MyPlants';
+import Search from './screens/Search';
+
+
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1}}>
+    <NavigationContainer>
+      
+      <Tab.Navigator 
+            initialRouteName='Home'
+            screenOptions={{
+            headerStyle: {
+            backgroundColor: '#fff',
+              },
+            headerTintColor: '#000',
+            headerTitleStyle: {
+            fontWeight: 'bold',
+              },
+            tabBarActiveTintColor: '#6495ed',
+
+      }}>
+        <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'Home' }} />
+        <Tab.Screen name="NewPlant" component={NewPlant}/>
+        <Tab.Screen name="MyPlants" component={MyPlants} />
+        <Tab.Screen name="Search" component={Search} />
+
+
+      </Tab.Navigator>
+  </NavigationContainer>
+  <StatusBar style="auto" />
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ screens/MyPlants.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { useState } from 'react';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is my plants</Text>
+      <StatusBar style="auto" />
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
